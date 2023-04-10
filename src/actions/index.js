eb332189fe9983d6d646e2100ceef42dc3da02f1
Atroopa <atroopa@gmail.com>
2023-04-10 16:10:16 +0430

    actions code work true

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+
+export const fetchPosts = (dispatch, getState) => {
+    return function () {
+        const response =  jsonPlaceHolder.get('/posts');
+
+        return {
+            type: 'FETCH_POSTS',
+            payload: response
+        };
+    }
 }
