3400780b44bb708208e74d6a424f12c16e50a333
Atroopa <atroopa@gmail.com>
2023-04-10 15:41:24 +0430

    create action

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
+import PostList from "./PostList";
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-5xl font-blod py-20 px-20">app</h1>
+      <PostList/>
     </div>
   );
 }
