a062ae7a72c76a4ea89e5e1eb6e26573014b6b19
Atroopa <atroopa@gmail.com>
2023-04-10 15:19:00 +0430

    config files and folder for react redux mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React        from 'react';
+import ReactDOM     from 'react-dom/client';
+import App          from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import './index.css';
-import App from './components/App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers)}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
