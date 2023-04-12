5614fcd24f9fb0e61bc518d50b945782053fb79e
Atroopa <atroopa@gmail.com>
2023-04-12 13:17:33 +0430

    create userheader components

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,15 @@ export const fetchPosts = () => async (dispatch, getState) => {
             payload: response.data
         });
 }
+
+export const fetchUser = id => async dispatch => {
+
+    const response = await jsonPlaceHolder.get(`./users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+
+}
+
